01bd830970da40f111eaab1d2d34de5e30b56965
= <=>
2025-05-13 18:50:24 +0200

    Skeleton added

--- hanged_man.py
+++ hanged_man.py
@@ -1,7 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 13 18:17:35 2025
+answer = 'Python'
 
-@author: Donald Kukfisz
-"""
+shots_fired = []
+chances = 5
 
+print('Gra w odgadywanie hasła.')
+while True:
+    
+    print()
+    shot = input('Wpisz literę: ')
+    shots_fired.append(shot)
+    
+    if shot not in answer:
+        chances -= 1
+    print()
+    print('Twoje postępy:')
+    for _ in answer:
+        if _.lower() in shots_fired:
+            print(f'{_} ', end= '')
+        else:
+            print('_ ', end = '')
+    
+    print()
+    print(f'Pozostałe szanse: {chances}')
+    
+    if chances == 0:
+        print('Przegrałes!')
+        break
+            
