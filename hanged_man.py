0a64ddb453ad78b4f2dadbefa7ce8506aaed8e22
= <=>
2025-05-14 17:09:43 +0200

    Dodano losowy wybór dwóch list ang/pol w oddzielnym pliku, dodano losowy wybor, dodano obsługę niewłasciwych znakow przy podawaniu liter git push git status git status git log

--- hanged_man.py
+++ hanged_man.py
@@ -1,6 +1,10 @@
 import sys
+import string
+import random
+from words_to_guess import english_words, polish_words
 
-word = 'python'
+
+word = random.choice(english_words)
 
 allready_used_letters = []
 user_answers = []
@@ -18,40 +22,54 @@ def find_index(word, letter):
             indexes.append(index)
     return indexes
 
-
-
-print('Gra w odgadywanie hasła.')
+print('Gra w odgadywanie słów. Wybierz w jakim języku chcesz odgadywać słowo:')
 while True:
     
-    print()
-    letter = input('Wpisz literę: ')
-    
-    if letter in allready_used_letters:
-        print('Litera już wykorzystana, spróbuj ponownie!')
+    language = input('1. Polski, 2. Angielski: ')
+    if language not in ('1', '2'):
+        print('Błąd, wybierz poprawny język:')
         continue
-    
-    allready_used_letters.append(letter)
-    
-    indexes = find_index(word, letter)
-    
-    
-    if len(find_index(word, letter)) == 0:
-        print('Nie ma takiej litery!')
-        chances -= 1
-        print(f'Pozostałe szanse: {chances}')
+    if language == 1:
+        word = random.choice(polish_words)
+    elif language == 2:
+        word = random.choice(english_words)
+
+    while True:
         
-        if chances == 0:
-            print('Koniec gry, przegrałes!')
-            sys.exit(0)
+        print()
+        letter = input('Wpisz literę: ')
+        
+        if letter not in (string.ascii_letters):
+            print('To nie jest pojedyncza litera, spróbuj jeszcze raz!')
+            continue
+        elif letter in allready_used_letters:
+            print('Litera już wykorzystana, spróbuj ponownie!')
+            continue
+        
+        allready_used_letters.append(letter)
+        
+        indexes = find_index(word, letter)
+        
+        
+        if len(find_index(word, letter)) == 0:
+            print('Nie ma takiej litery!')
+            chances -= 1
+            print(f'Pozostałe szanse: {chances}')
             
-    else:
-        
-        for index in indexes:
-            user_answers[index] = letter
-        
-        if ''.join(user_answers) == word:
-            print('Gratulacje, odgadłes słowo!')
-            sys.exit(0)
-    
-    print(user_answers)
+            if chances == 0:
+                print('Koniec gry, przegrałes! Hasło brzmiało: ', word)
+                sys.exit(0)
+                
+        else:
             
+            for index in indexes:
+                user_answers[index] = letter
+            
+            if ''.join(user_answers) == word:
+                print('Gratulacje, odgadłes słowo! Odpowiedź brzmi: ', word)
+                sys.exit(0)
+        
+        print(user_answers)
+    
+    
+ 
---
+++ words_to_guess.py
@@ -0,0 +1,26 @@
+english_words = [
+    "apple", "banana", "orange", "grape", "peach", "cherry", "lemon", "melon", "plum", "pear",
+    "table", "chair", "window", "mirror", "carpet", "lamp", "couch", "shelf", "closet", "desk",
+    "house", "garden", "garage", "kitchen", "bedroom", "bathroom", "hallway", "balcony", "roof", "door",
+    "school", "student", "teacher", "lesson", "classroom", "pencil", "notebook", "book", "board", "subject",
+    "computer", "mouse", "keyboard", "monitor", "printer", "screen", "speaker", "camera", "router", "cable",
+    "dog", "cat", "rabbit", "hamster", "parrot", "turtle", "fish", "horse", "cow", "sheep",
+    "river", "mountain", "forest", "desert", "valley", "ocean", "island", "volcano", "cave", "hill",
+    "train", "bus", "bicycle", "airplane", "ship", "car", "motorcycle", "subway", "truck", "scooter",
+    "doctor", "nurse", "patient", "hospital", "clinic", "medicine", "surgery", "bandage", "vaccine", "ambulance",
+    "happy", "sad", "angry", "afraid", "excited", "bored", "tired", "proud", "nervous", "calm"
+]
+
+polish_words = [
+    "ananas", "balon", "banan", "baran", "bilet", "biurko", "cebula", "cukier", "czapka", "czekolada",
+    "deska", "drabina", "droga", "durszlak", "dym", "dzban", "dzialka", "dziennik", "farba", "filiżanka",
+    "fotel", "garnek", "gazeta", "glowa", "gruszka", "hamak", "herbata", "historia", "instrument", "iskra",
+    "jablko", "jezioro", "jogurt", "kaloryfer", "kamera", "kapelusz", "kasza", "kawa", "klucz", "kolano",
+    "komputer", "kotek", "krzeslo", "ksiadz", "kwiat", "lampa", "lawka", "lis", "lody", "lustro",
+    "malpa", "mapa", "maslo", "materac", "miska", "miasto", "mleko", "mrowka", "mysz", "napoj",
+    "nozyczki", "notes", "obiad", "okno", "olej", "opona", "orech", "pajak", "papier", "parasol",
+    "pies", "piornik", "piwnica", "pociag", "pomidor", "pralka", "proszek", "rakieta", "rower", "rura",
+    "ryba", "samochod", "serce", "skrzynka", "slon", "snieg", "sofa", "spodnie", "sroda", "stolik",
+    "suszarka", "sweter", "szafa", "szalik", "szczotka", "szkoła", "talerz", "telefon", "tornister", "trawa",
+    "tygrys", "ubranie", "wanna", "wazon", "wiadro", "wieszak", "winda", "woda", "zegarek", "zeszyt"
+]
