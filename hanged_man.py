725641bfab7563be544e71337e56c527f77b9b41
= <=>
2025-05-14 15:53:27 +0200

    Zaimplementowano surową wersję, działającą w idealnym scenariuszu

--- hanged_man.py
+++ hanged_man.py
@@ -1,32 +1,57 @@
-answer = 'Python'
+import sys
 
-shots_fired = []
+word = 'python'
+
+allready_used_letters = []
+user_answers = []
 chances = 5
+
+for letter in word:
+    user_answers.append('_')
+
+def find_index(word, letter):
+    
+    indexes = []
+    
+    for index, letter_in_answer in enumerate(word):
+        if letter == letter_in_answer:
+            indexes.append(index)
+    return indexes
+
+
 
 print('Gra w odgadywanie hasła.')
 while True:
     
     print()
-    shot = input('Wpisz literę: ')
-    shots_fired.append(shot)
+    letter = input('Wpisz literę: ')
     
-    if shot not in answer:
+    if letter in allready_used_letters:
+        print('Litera już wykorzystana, spróbuj ponownie!')
+        continue
+    
+    allready_used_letters.append(letter)
+    
+    indexes = find_index(word, letter)
+    
+    
+    if len(find_index(word, letter)) == 0:
+        print('Nie ma takiej litery!')
         chances -= 1
-    print()
-    print('Twoje postępy:')
-    for _ in answer:
-        if _.lower() in shots_fired:
-            print(f'{_} ', end= '')
-        else:
-            print('_ ', end = '')
+        print(f'Pozostałe szanse: {chances}')
+        
+        if chances == 0:
+            print('Koniec gry, przegrałes!')
+            sys.exit(0)
+            
+    else:
+        
+        for index in indexes:
+            user_answers[index] = letter
+        
+        if ''.join(user_answers) == word:
+            print('Gratulacje, odgadłes słowo!')
+            sys.exit(0)
     
-    print()
-    print(f'Pozostałe szanse: {chances}')
-    
-    if chances == 0:
-        print('Przegrałes!')
-        break
-    if (sorted(set(answer))) == (sorted(set(shots_fired))):
-        print('Wygrałes!')
-        break
-    
+    print(user_answers)
+            
