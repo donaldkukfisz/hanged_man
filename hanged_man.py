b8066d33bf8af45e8151b197e5d068c72ca68add
= <=>
2025-05-19 16:19:28 +0200

    Dodano poziomy trudności, poprawiono logikę związanią z resetowaniem hasła po rozpoczynaniu gry jeszcze raz

--- hanged_man.py
+++ hanged_man.py
@@ -3,15 +3,7 @@ import string
 import random
 from words_to_guess import english_words, polish_words
 
-
-word = random.choice(english_words)
-
-allready_used_letters = []
-user_answers = []
-chances = 5
-
-for letter in word:
-    user_answers.append('_')
+chances = 0
 
 def find_index(word, letter):
     
@@ -22,22 +14,55 @@ def find_index(word, letter):
             indexes.append(index)
     return indexes
 
-print('Gra w odgadywanie słów. Wybierz w jakim języku chcesz odgadywać słowo:')
+def choose_difficulty():
+    
+        
+    level = input('Wybierz poziom trudnosci: 1. ŁATWY, 2.ŚREDNI, 3.TRUDNY: ')
+    
+    if level == '1':
+        chances = 10
+        msg = f'Wybrano poziom ŁATWY. Masz {chances} prób odgadnięcia!'
+    elif level == '2':
+        chances = 5
+        msg = f'Wybrano poziom ŚREDNI. Masz {chances} prób odgadnięcia!'
+    elif level == '3':
+        chances = 3
+        msg = f'Wybrano poziom TRUDNY. Masz {chances} prób odgadnięcia!'
+        
+    return chances, msg
+    
+
+print('Gra w odgadywanie słów!')
+
+
 while True:
     
-    language = input('1. Polski, 2. Angielski: ')
+    chances = 0
+    chances, msg = choose_difficulty()
+    print(msg)
+    
+    language = input('Wybierz język w jakim chcesz odgadnąc słowo: 1. Polski, 2. Angielski: ')
+
     if language not in ('1', '2'):
         print('Błąd, wybierz poprawny język:')
         continue
-    if language == 1:
+    if language == '1':
         word = random.choice(polish_words)
-    elif language == 2:
+    elif language == '2':
         word = random.choice(english_words)
-
+    
+    allready_used_letters = []
+    user_answers = []
+    
+    for letter in word:
+        user_answers.append('_')
+    
+    print('Słowo zostało wylosowane!')
+    
     while True:
         
         print()
-        letter = input('Wpisz literę: ')
+        letter = input('Odgadnij literę i zatwierdz ENTEREM: ')
         
         if letter not in (string.ascii_letters):
             print('To nie jest pojedyncza litera, spróbuj jeszcze raz!')
@@ -57,16 +82,22 @@ while True:
             print(f'Pozostałe szanse: {chances}')
             
             if chances == 0:
-                print('Koniec gry, przegrałes! Hasło brzmiało: ', word)
-                sys.exit(0)
+                print(f'Koniec gry, przegrałes! Hasło brzmiało: {word.upper()}')
+                print()
+                end = input('Czy chcesz zagrać jeszcze raz? T/N: ')
+                if end.lower() == 't':
+                    break
                 
+                else:
+                    print('Koniec gry, do zobaczenia!')
+                    sys.exit(0)
         else:
             
             for index in indexes:
                 user_answers[index] = letter
             
             if ''.join(user_answers) == word:
-                print('Gratulacje, odgadłes słowo! Odpowiedź brzmi: ', word)
+                print(f'GRATULACJE! Odgadłes słowo! Odpowiedź brzmi: {word.upper()}')
                 sys.exit(0)
         
         print(user_answers)
